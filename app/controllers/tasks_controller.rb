e58c84e268ce6eaa40fa987582cfe2f6f3cd7e40
renaud archer fortin <renaud.archerf@gmail.com>
2020-11-10 12:57:19 -0500

    create works

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,12 +5,17 @@ class TasksController < ApplicationController
  end
 
  def show
+  @task = Task.find(params[:id])
  end
 
  def new
+  @task = Task.new
  end
 
  def create
+  @task = Task.new(task_params)
+  @task.save
+  redirect_to tasks_path
  end
 
  def edit
@@ -22,4 +27,7 @@ class TasksController < ApplicationController
  def destroy
  end
 
+ def task_params
+  params.require(:task).permit(:title, :details)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show'
   get '/tasks/new', to: 'tasks#new'
+  get '/tasks/:id', to: 'tasks#show', as: :task 
   post '/tasks', to: 'tasks#create'
   get '/tasks/:id/edit', to: 'tasks#edit'
   patch '/tasks/:id', to: 'tasks#update'
