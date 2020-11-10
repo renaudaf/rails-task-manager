0512a9abcb4c032315e8006539d5528cd14488fd
renaud archer fortin <renaud.archerf@gmail.com>
2020-11-10 13:40:47 -0500

    update and delete work

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,12 +19,19 @@ class TasksController < ApplicationController
  end
 
  def edit
+  @task = Task.find(params[:id])
  end
 
  def update
+  @task = Task.find(params[:id])
+  @task.update(task_params)
+  redirect_to tasks_path
  end
 
  def destroy
+  @task = Task.find(params[:id])
+  @task.destroy
+  redirect_to tasks_path
  end
 
  def task_params
